0e5a627b8fe3fce797f5238338e56111791b421e
nuraiymkadyralieva <parkjamie576@gmail.com>
2026-03-02 17:22:27 +0600

    init: spring boot project

---
+++ src/main/java/org/example/businessprocessservice/BusinessProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.businessprocessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessProcessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessProcessServiceApplication.class, args);
+    }
+
+}
